0d36b8c3671cd6b7f3e3e2e222fab82e213e886e
Петър Стоев <66165404+PetarStoev02@users.noreply.github.com>
2022-06-02 19:01:32 +0300

    Add files via upload

---
+++ Syntax, Functions and Statements - Lab/01. Echo Function.js
@@ -0,0 +1,9 @@
+function echo(myStr){
+
+  let stringLength = myStr.length;
+  console.log(stringLength);
+  console.log(myStr);
+}
+
+echo('Hello, JavaScript!');
+echo('strings are easy');
---
+++ Syntax, Functions and Statements - Lab/03. Largest Number.js
@@ -0,0 +1,16 @@
+function firstSolution(x,y,z){
+    let result;
+                      
+    if(x >= y && x >= z){
+        result=x;
+    }else if(y >=x && y>=z){
+        result=y;
+    }else{
+        result=z;
+   }
+    console.log(`The largest number is${result}.`);
+}
+
+function secondSolution(x,y,z){
+    console.log(`The largest number is${Math.max(x,y,z)}.`);
+}
